b81f8ef4d9437c3cc7d4db928062d86d0662eabd
Raquel Díaz González <raquel.diazgonz@alumnos.upm.es>
2015-09-30 17:12:58 +0200

    Creada una suite de test y modificado DecimalCollectionTest

--- src/test/java/es/upm/miw/spai/ecp2/DecimalCollectionTest.java
+++ src/test/java/es/upm/miw/spai/ecp2/DecimalCollectionTest.java
@@ -17,22 +17,22 @@ public class DecimalCollectionTest {
     @Test
     public void addTest() {
         assertEquals(0, decimalCollection.size());
-        decimalCollection.add(1);
+        decimalCollection.add(1.14);
         assertEquals(1, decimalCollection.size());
-        decimalCollection.add(2);
+        decimalCollection.add(12.65);
         assertEquals(2, decimalCollection.size());
-        decimalCollection.add(3);
+        decimalCollection.add(5.78);
         assertEquals(3, decimalCollection.size());
     }
 
     @Test
     public void sizeTest() {
         assertEquals(0, decimalCollection.size());
-        decimalCollection.add(1);
+        decimalCollection.add(18.29);
         assertEquals(1, decimalCollection.size());
-        decimalCollection.add(2);
+        decimalCollection.add(56.215);
         assertEquals(2, decimalCollection.size());
-        decimalCollection.add(3);
+        decimalCollection.add(3.1789);
         assertEquals(3, decimalCollection.size());
     }
 
@@ -50,11 +50,11 @@ public class DecimalCollectionTest {
     @Test
     public void higherTest() {
         assertEquals(Double.NEGATIVE_INFINITY, decimalCollection.higher(), 10e-3);
-        decimalCollection.add(1);
-        assertEquals(1.0, decimalCollection.higher(), 10e-3);
+        decimalCollection.add(1.17);
+        assertEquals(1.17, decimalCollection.higher(), 10e-3);
         decimalCollection.add(0.5);
-        assertEquals(1.0, decimalCollection.higher(), 10e-3);
-        decimalCollection.add(3);
-        assertEquals(3.0, decimalCollection.higher(), 10e-3);
+        assertEquals(1.17, decimalCollection.higher(), 10e-3);
+        decimalCollection.add(35.89);
+        assertEquals(35.89, decimalCollection.higher(), 10e-3);
     }
 }
